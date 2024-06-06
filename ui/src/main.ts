2fd8cc7cf60f7f3c6f6081498d4f78f210fab548
Stephan Merker <merker.stephan@gmail.com>
2024-06-06 22:47:27 +0200

    Update to angular 18
    - enable eventCoalescing

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,9 +3,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppComponent } from './app/app.component';
 import { statusInterceptor } from './app/http-status.service';
+import { provideZoneChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(withInterceptors([statusInterceptor])),
     provideAnimationsAsync()
   ]
