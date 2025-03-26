8fd37e5867d32878678e756ffa268113194f7be1
Stephan Merker <merker.stephan@gmail.com>
2025-03-26 08:42:12 +0100

    Improve resilience of update-pvcontrol
    - failed download shall not break existing installation

--- update-pvcontrol.sh
+++ update-pvcontrol.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
-set -x
+set -ex
 
 # Usage: update-pvcontrol.sh [version]
 # Updates pvcontrol installed in /usr/local/bin to latest build or to the specified version.
 # assumes valid gh login (e.g. gh auth login -w)
 
-rm pv-control.tar.gz
-rm -rf pvcontrol
-sudo rm -rf /usr/local/bin/pvcontrol-old
+rm -f pv-control.tar.gz
 if [ -z "$1" ]; then
     gh run download -R stephanme/pv-control -n pv-control.tar.gz
 else
     gh release download "$1" -R stephanme/pv-control --pattern 'pv-control.tar.gz' --output pv-control.tar.gz
 fi
+rm -rf pvcontrol
 mkdir -p pvcontrol && tar -xzf pv-control.tar.gz -C ./pvcontrol
 ~/.env/bin/pip install -r pvcontrol/requirements.txt
+sudo rm -rf /usr/local/bin/pvcontrol-old
 sudo mv /usr/local/bin/pvcontrol /usr/local/bin/pvcontrol-old
 sudo mv ./pvcontrol /usr/local/bin/pvcontrol
 sudo systemctl restart pvcontrol.service
